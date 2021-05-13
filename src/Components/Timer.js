93f8f86d4c67ba1ddd8438f8b5a2ab3b7a3198d9
aprillialee <aprilliapmlee@gmail.com>
2021-05-13 14:56:48 +0100

    moved countdown clock and added header and metadata

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -73,15 +73,17 @@ const TimerStyled = styled.div`
   flex-direction: row nowrap;
   justify-content: space-evenly;
   align-items: center;
-  width: 65%;
-  height: 25%;
+  margin-top: -60px;
+  width: 35%;
+  height: 15%;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   background: rgba(255, 255, 255, 0.35);
   border-radius: 8px;
   z-index: 100;
 
   @media (max-width: 1024px) {
-    height: 18%;
+    height: 10%;
+    width: 70%;
   }
 `;
 
@@ -90,25 +92,29 @@ const TimerContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 100px;
+  width: 50px;
   height: 100%;
 `;
 
 const Time = styled.h1`
   margin: 0;
   padding: 0;
-  font-size: 7vw;
+  font-size: 4vw;
   color: white;
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
 `;
 
 const Text = styled.h2`
   margin: 0;
   padding: 0;
-  font-size: 1vw;
+  font-size: 0.8vw;
   color: white;
 
   @media (max-width: 768px) {
-    font-size: 10px;
+    font-size: 12px;
   }
 `;
 
